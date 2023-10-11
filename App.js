68ababa9385eb3d9b92ec31889f70088cac5bd66
Venomity <veeti.hietasalo@hotmail.fi>
2023-10-11 15:54:25 +0300

    palautus

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, TextInput, Button, StyleSheet } from 'react-native';
+import * as Speech from 'expo-speech';
 
-export default function App() {
+const App = () => {
+  const [text, setText] = useState('');
+
+  const speakText = () => {
+    Speech.speak(text);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        style={styles.inputField}
+        placeholder="Kirjoita teksti tähän"
+        onChangeText={text => setText(text)}
+        value={text}
+      />
+      <Button title="Puhu teksti" onPress={speakText} />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+  },
+  inputField: {
+    borderWidth: 1,
+    borderColor: 'grey',
+    padding: 10,
+    marginBottom: 20,
   },
 });
+
+export default App;
